39fb3fcbfc454dc0d0dd7802ce69a395b004e277
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-12-04 14:40:48 +0100

    114 done

---
+++ 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadSumMatrix.java
@@ -0,0 +1,103 @@
+package it.unibo.oop.workers02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class MultiThreadSumMatrix implements SumMatrix{
+    private final int nthread;
+    /**
+     * 
+     * @param nthread
+     *            no. of thread performing the sum.
+     */
+    public MultiThreadSumMatrix(final int nthread) {
+        this.nthread = nthread;
+    }
+
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private double res;
+
+        /**
+         * Build a new worker.
+         * 
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+
+            this.res = IntStream.range(startpos, matrix.length)
+                .filter(n -> startpos < nelem)
+                .mapToDouble(
+                    i -> IntStream.range(0, matrix.length)
+                        .mapToDouble(j -> matrix[i][j])
+                        .sum()
+                ).sum();                
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         * 
+         * @return the sum of every element in the array
+         */
+        public double getResult() {
+            return this.res;
+        }
+
+    }
+
+    @Override
+    public double sum(final double[][] matrix) {
+        final int size = matrix.length % nthread + matrix.length / nthread;
+        /*
+         * Build a list of workers
+         */
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        /*
+         * Start them
+         */
+        for (final Worker w: workers) {
+            w.start();
+        }
+        /*
+         * Wait for every one of them to finish. This operation is _way_ better done by
+         * using barriers and latches, and the whole operation would be better done with
+         * futures.
+         */
+        double sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        /*
+         * Return the sum
+         */
+        return sum;
+    }
+}
+
+
